4de7fe1ece9de210f010b3ed84b11c16cfb9cba0
Roy Portas <royportas@gmail.com>
2017-04-27 11:55:13 +1000

    Added start of software

---
+++ assignment2/crime-globe/src/components/WebGL.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>WebGL</h1>
+    <canvas class="webgl-canvas" ref="canvas">
+    </canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebGL',
+  data() {
+    return {
+      gl: null,
+    };
+  },
+
+  methods: {
+    initGL() {
+      const canvas = this.$refs.canvas;
+
+      this.gl = canvas.getContext('webgl') || canvas.getContent('experimental-webgl');
+
+      // Set clear color to black
+      this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+      // Enable depth testing
+      this.gl.enable(this.gl.DEPTH_TEST);
+      // Near things obscure far things
+      this.gl.depthFunc(this.gl.LEQUAL);
+      // Clear the color as well as the depth buffer
+      this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    },
+  },
+
+  mounted() {
+    this.initGL();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.webgl-canvas {
+  width: 90%;
+  height: 300px;
+}
+
+</style>
